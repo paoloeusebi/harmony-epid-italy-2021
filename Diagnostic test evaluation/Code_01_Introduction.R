a1a4045ca36242649d4b0a616ac001c40ba58007
Paolo Eusebi <paoloeusebi@gmail.com>
2021-08-18 00:10:05 +0200

    format updated

---
+++ Diagnostic test evaluation/Code_01_Introduction.R
@@ -0,0 +1,33 @@
+library(runjags)
+library(rjags)
+testjags()
+
+
+# Bayesian model
+basicjags <- "model{
+  # Likelihood part:
+  Positives ~ dbinom(prevalence, TotalTests)
+
+  # Prior part:
+  prevalence ~ dbeta(2, 2)
+
+  # Hooks for automatic integration with R:
+  #data# Positives, TotalTests
+  #monitor# prevalence
+  #inits# prevalence
+}
+"
+
+Positives <- 7
+TotalTests <- 10
+
+# initial values to be retrieved by runjags:
+prevalence <- list(chain1=0.05, chain2=0.95)
+
+# Running
+results <- run.jags(basicjags,
+                    n.chains = 2,
+                    burnin = 1000,
+                    sample = 5000)
+print(results)
+plot(results)
