9c46fd338028aab730bef1bc87336cc0d2990655
Paolo Eusebi <paoloeusebi@gmail.com>
2021-09-12 16:24:57 +0200

    added SEIR and cleaning

--- S02_Compartmental models/Code_01_Compartmental models.R
+++ S02_Compartmental models/Code_01_Compartmental models.R
@@ -1,20 +1,19 @@
 #Load required libraries
 library(tidyverse)
 library(deSolve)
-library(reshape2)
-
-# Model inputs
-
-initial_state_values=c(S=999999,I=1,R=0)
-parameters=c(gamma=0.20,beta=0.5)
 
 # Time points
 
-time=seq(from=1,to=100,by=1)
+time <- seq(from=1, to=100, by=1)
 
-# SIR model function
+# SIR Model ---------------------------------------------------------------
 
-sir_model <- function(time,state,parameters){
+## model inputs
+initial_state_values=c(S=999999,I=1,R=0)
+parameters=c(gamma=0.20,beta=0.5)
+
+## SIR model function
+sir_model <- function(time, state, parameters){
   with(as.list(c(state,parameters)),{
     N=S+I+R
     lambda=beta*(I/N)
@@ -27,8 +26,7 @@ sir_model <- function(time,state,parameters){
   )
 }
 
-
-#Solving the differential equations
+## solving the differential equations
 output <- ode(
   y = initial_state_values,
   func = sir_model,
@@ -37,14 +35,70 @@ output <- ode(
   ) %>%
   as.data.frame()
 
+output
+
 output2 <- output %>%
   gather(key = compartment, value = count, S:R)
 
-# To plot the proportion of susceptible, infected and recovered individuals over time
+output2
+
+## plot the proportion of states (S, I, R) over time
 ggplot(data = output2,
-       aes(x = time, y = count/1000000, fill = compartment)) +
+       aes(x = time, y = count/sum(initial_state_values), fill = compartment)) +
   geom_area(col="white") +
   scale_fill_manual(values = c("firebrick", "deepskyblue4", "yellowgreen")) +
   xlab("Time (days)") +
   ylab("Proportion of the population") +
   scale_color_discrete(name = "State")
+
+
+# SEIR Model --------------------------------------------------------------
+## model inputs
+initial_state_values=c(S=500, E=499, I=1, R=0)
+parameters=c(gamma=0.20, beta=0.5, R0=2, sigma=1)
+
+## SEIR model function
+seir_model <- function(time, state, parameters){
+  with(as.list(c(state,parameters)),{
+    dS <- -(R0*gamma) * S * I/(S + E + I + R)
+    dE <- (R0*gamma) * S * I/(S + E + I + R) - sigma * E
+    dI <- sigma * E - gamma * I
+    dR <- gamma * I
+
+    return(list(c(dS, dE, dI, dR)))
+  }
+  )
+}
+
+## solving the differential equations
+output <- ode(
+  y = initial_state_values,
+  func = seir_model,
+  parms = parameters,
+  times = time
+  ) %>%
+  as.data.frame()
+
+output
+
+output2 <- output %>%
+  gather(key = compartment, value = count, S:R)
+
+output2
+
+## plot the proportion of states (S, E, I, R) over time
+ggplot(data = output2,
+       aes(x = time, y = count/sum(initial_state_values), fill = compartment)) +
+  geom_area(col="white") +
+  scale_fill_manual(values = c("violet","firebrick", "deepskyblue4", "yellowgreen")) +
+  xlab("Time (days)") +
+  ylab("Proportion of the population") +
+  scale_color_discrete(name = "State")
+
+
+# Exercise ----------------------------------------------------------------
+
+# Run previous models changing the parameters
+# Simulate a reasonable model (SIR and/or SEIR) for an epidemics of interest
+
+
